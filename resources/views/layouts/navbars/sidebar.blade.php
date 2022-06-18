df88ffdda966700578f84f67fe5f654d29cfdee6
voquangsinh <voquangsinh@gmail.com>
2022-06-18 21:02:39 +0700

    sidebar menu

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -77,28 +77,7 @@
                 </div>
             </form>
             <!-- Navigation -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('home') }}">
-                        <i class="ni ni-tv-2 text-primary"></i> {{ __('Dashboard') }}
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('vidu')}}">
-                        <i class="ni ni-circle-08 text-pink"></i> {{ __('Vidu') }}
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="ni ni-circle-08 text-pink"></i> {{ __('Posts') }}
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="ni ni-circle-08 text-pink"></i> {{ __('Categories') }}
-                    </a>
-                </li>
-            </ul>
+            @include('route-menu')
         </div>
     </div>
 </nav>
